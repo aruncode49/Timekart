c80ad26c2d58f05e7226baa471d704cf7ef28d1b
Arun Kumar <aruncode49@gmail.com>
2023-09-22 17:25:45 +0530

    Ecommerce Frontend Project Setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Layout from "./components/layouts/Layout";
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <h1 className="text-red-500">Ecommerce App</h1>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
