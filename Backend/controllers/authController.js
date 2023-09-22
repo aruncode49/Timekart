219e72afe13617a80ec649b7d1d5c509279344f3
Arun Kumar <aruncode49@gmail.com>
2023-09-22 15:04:52 +0530

    Login Controller, Created JWT

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 import { hashPassword, comparePassword } from "../helpers/bcrypt.js";
 
@@ -21,6 +22,53 @@ export const registerController = async (req, res) => {
     res.status(500).send({
       success: false,
       message: "Error in registration",
+      error,
+    });
+  }
+};
+
+// login controller
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    // For Wrong Email
+    if (!user) {
+      return res.status(200).send({
+        success: false,
+        message: "Invalid Email",
+      });
+    }
+
+    // If Email is Correct then Check Password
+    const hashedPassword = user.password;
+    const isPasswordMatch = await comparePassword(password, hashedPassword);
+
+    if (!isPasswordMatch) {
+      return res.status(200).send({
+        success: false,
+        message: "Invalid Password",
+      });
+    }
+
+    // If Both are Correct then create a json web token
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    return res.status(200).send({
+      success: true,
+      message: "User successfully logged in",
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(`Error inside loginController : ${error}`);
+    res.status(500).send({
+      success: false,
+      message: "Error in login",
       error,
     });
   }
--- Backend/helpers/bcrypt.js
+++ Backend/helpers/bcrypt.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 // Password Hashing
 export const hashPassword = async (password) => {
   try {
-    const saltRound = await bcrypt.genSalt();
+    const saltRound = 10;
     const hashedPassword = await bcrypt.hash(password, saltRound);
     return hashedPassword;
   } catch (error) {
@@ -14,7 +14,7 @@ export const hashPassword = async (password) => {
 // Compare Password
 export const comparePassword = async (password, hashedPassword) => {
   try {
-    return await bcrypt.compare(password, hashPassword);
+    return await bcrypt.compare(password, hashedPassword);
   } catch (error) {
     console.log(`Error in comparePassword (bcrypt.js) : ${error}`);
   }
