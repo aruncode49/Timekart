0bc236b5a5ac2553c0027432c947847b966fd72a
Arun Kumar <aruncode49@gmail.com>
2023-09-23 18:27:13 +0530

    About Page Complete

--- Frontend/src/components/pages/About.jsx
+++ Frontend/src/components/pages/About.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Layout from "../layouts/Layout";
+import aboutImage from "/images/about.jpg";
 
 const About = () => {
   return (
     <Layout>
-      <div>About</div>
+      <div className="md:flex items-center justify-center gap-4 pb-10">
+        <img className="mx-auto w-[400px]" src={aboutImage} alt="About Logo" />
+        <p className="text-lg text-center">
+          "Elevate your online shopping experience with our cutting-edge
+          e-commerce platform, powered by the robust MERN stack (MongoDB,
+          Express.js, React.js, and Node.js). We've seamlessly combined the
+          power of these technologies to bring you a feature-rich, user-friendly
+          platform. Plus, we've enhanced your shopping journey with sleek and
+          modern aesthetics, thanks to our use of Tailwind CSS for a visually
+          stunning experience."
+        </p>
+      </div>
     </Layout>
   );
 };
