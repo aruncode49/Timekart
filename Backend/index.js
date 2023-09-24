35904b80e5fc59885781504c31437a8702c829c9
Arun Kumar <aruncode49@gmail.com>
2023-09-24 16:04:52 +0530

    Running Frontend and Backend Concurrently

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import authRoutes from "./routes/authRoute.js";
 import { connectMongoDb } from "./config/db.js";
@@ -19,6 +20,7 @@ connectMongoDb();
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 // routes
 app.use("/api/v1/auth", authRoutes);
