e669646c00726c4ce67d21364c5213fabcacdf0e
Arun Kumar <aruncode49@gmail.com>
2023-09-28 14:23:17 +0530

    Category Update in Backend

--- Backend/controllers/categoryContoller.js
+++ Backend/controllers/categoryContoller.js
@@ -24,11 +24,31 @@ export async function createCategoryController(req, res) {
       message: "category created successfully",
     });
   } catch (error) {
-    console.log(`Error inside category controller: ${error}`);
+    console.log(`Error inside create category controller: ${error}`);
     return res.status(500).send({
       error,
       success: false,
-      message: "Error inside cateogry controller",
+      message: "Error inside create cateogry controller",
+    });
+  }
+}
+
+// update category controller
+export async function updateCategoryController(req, res) {
+  try {
+    const { name } = req.body;
+    const categoryId = req.params.id;
+    await Category.findByIdAndUpdate(categoryId, { name, slug: slugify(name) });
+    return res.status(200).send({
+      success: true,
+      message: "Category Updated Successfully",
+    });
+  } catch (error) {
+    console.log(`Error inside update category controller: ${error}`);
+    return res.status(500).send({
+      error,
+      success: false,
+      message: "Error inside update cateogry controller",
     });
   }
 }
--- Backend/routes/categoryRoute.js
+++ Backend/routes/categoryRoute.js
@@ -1,15 +1,20 @@
 import express from "express";
 import { requireLogin, isAdmin } from "../middlewares/authMiddleware.js";
-import { createCategoryController } from "../controllers/categoryContoller.js";
+import {
+  createCategoryController,
+  updateCategoryController,
+} from "../controllers/categoryContoller.js";
 
 const router = express.Router();
 
 // post route for create category
-router.post(
-  "/create-category",
-  requireLogin,
-  isAdmin,
-  createCategoryController
-);
+router.post("/create", requireLogin, isAdmin, createCategoryController);
+
+// put route for update category
+router.put("/update/:id", requireLogin, isAdmin, updateCategoryController);
+
+// get route for getting all categories
+// get route for getting a particular category
+// delete route for deleting a particular category
 
 export default router;
