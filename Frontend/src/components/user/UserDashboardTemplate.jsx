8a9782e8813c691abddbbc3af754a2cd5d9ae20b
Arun Kumar <aruncode49@gmail.com>
2023-10-10 14:47:55 +0530

    User dashboard style refactor

--- Frontend/src/components/user/UserDashboardTemplate.jsx
+++ Frontend/src/components/user/UserDashboardTemplate.jsx
@@ -14,11 +14,11 @@ const UserDashboardTemplate = ({ children, title }) => {
     <Layout title={title}>
       <div className="max-w-screen-lg mx-auto">
         {/* Admin Panel Heading */}
-        <div
-          onClick={handleSideBarToggler}
-          className="flex items-center justify-center gap-2 border-b py-2 cursor-pointer text-2xl font-medium mt-3 bg-gray-200 w-12 mb-2 rounded-lg"
-        >
-          {isOpen ? <AiFillCaretUp /> : <AiFillCaretDown />}
+        <div onClick={handleSideBarToggler} className="flex items-center gap-2">
+          <div className="flex items-center justify-center gap-2 border-b py-2 cursor-pointer text-2xl font-medium mt-3 bg-gray-200 w-12 mb-2 rounded-lg">
+            {isOpen ? <AiFillCaretUp /> : <AiFillCaretDown />}
+          </div>
+          <h1 className="text-xl font-medium">User Dashboard</h1>
         </div>
         <div className="flex gap-3">
           {/* Side Menu */}
--- Frontend/src/components/user/UserSideBar.jsx
+++ Frontend/src/components/user/UserSideBar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 
 const UserSideBar = () => {
   return (
-    <div className="w-[200px] h-[300px] fixed md:relative bg-white">
+    <div className="w-[200px] fixed md:relative bg-white">
       <>
         <ul className="flex flex-col">
           <NavLink
