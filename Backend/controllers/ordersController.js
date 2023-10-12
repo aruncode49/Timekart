353cb1f4b41e9414b054e540fee00de25319d6fd
Arun Kumar <aruncode49@gmail.com>
2023-10-12 15:46:33 +0530

    User orders page is complete

--- Backend/controllers/ordersController.js
+++ Backend/controllers/ordersController.js
@@ -6,7 +6,11 @@ export const userOrdersController = async (req, res) => {
     const orders = await Order.find({ buyer: req.userId })
       .populate("products", "-photo")
       .populate("buyer", "name");
-    res.json(orders);
+
+    res.status(200).send({
+      success: true,
+      orders,
+    });
   } catch (error) {
     console.log(`Error insdie userOrdersController: ${error}`);
   }
--- Frontend/src/components/user/UserOrdersPage.jsx
+++ Frontend/src/components/user/UserOrdersPage.jsx
@@ -1,10 +1,95 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import moment from "moment";
 import UserDashboardTemplate from "./UserDashboardTemplate";
+import useAuth from "../../context/AuthContext";
 
 const UserOrdersPage = () => {
+  const [orders, setOrders] = useState([]);
+  const [auth, setAuth] = useAuth();
+
+  async function getAllOrders() {
+    try {
+      const { data } = await axios.get("/api/v1/order/all-orders");
+      if (data?.success) {
+        setOrders(data?.orders);
+      }
+    } catch (error) {
+      console.log(`Error inside get all order function : ${error}`);
+    }
+  }
+
+  useEffect(() => {
+    if (auth?.user) getAllOrders();
+  }, []);
+
   return (
     <UserDashboardTemplate title={"User Orders - Deal Daddy"}>
-      UserOrdersPage
+      <div className="w-full">
+        <h1 className="text-2xl mt-2 font-medium text-center">Your Orders</h1>
+
+        {/* Orders Details */}
+        {orders.length > 0 ? (
+          <table className="w-full border border-black mt-3">
+            <thead>
+              <tr className="text-center bg-yellow-300 text-sm font-normal">
+                <th className="py-3">Sr.No.</th>
+                <th>Status</th>
+                <th>Buyer</th>
+                <th>Date</th>
+                <th>Payment</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order, index) => (
+                <tr key={index} className="text-center text-sm">
+                  <td className="py-3">{index + 1}</td>
+                  <td>{order?.status}</td>
+                  <td>{order?.buyer?.name}</td>
+                  <td>{moment(order?.createdAt).fromNow()}</td>
+                  <td>{order?.payment?.success ? "Success" : "Failed"}</td>
+                  <td>{order?.products?.length}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p className="text-center mt-4 text-red-500">No orders to display.</p>
+        )}
+
+        {/* Ordered Products */}
+        <div>
+          {orders[0]?.products?.map((product) => (
+            <div
+              key={product._id}
+              className="flex border mt-6 shadow-md border-gray-400 gap-x-6 bg-gray-100 rounded-lg items-center p-2"
+            >
+              <div className="">
+                <img
+                  className="max-w-[70px] rounded-lg"
+                  src={`/api/v1/product/image/${product._id}`}
+                  alt={`${product.name} image`}
+                />
+              </div>
+              <div className="mt-2">
+                <h1 className="line-clamp-1 text-start font-medium text-gray-700">
+                  {product.name}
+                </h1>
+                <p className="line-clamp-1 text-start text-gray-600">
+                  {product.description}
+                </p>
+                <p className="text-start">
+                  Price:{" "}
+                  <span className="font-medium text-green-700">
+                    ₹{product.price}
+                  </span>
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </UserDashboardTemplate>
   );
 };
