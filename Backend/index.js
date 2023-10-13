17fbaa2ef6d05dc1ce2891b0e0c58beb715cff40
Arun Kumar <aruncode49@gmail.com>
2023-10-13 16:07:28 +0530

    backend refactor

--- Backend/index.js
+++ Backend/index.js
@@ -2,17 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoute.js";
 import ordersRoutes from "./routes/ordersRoute.js";
 import { connectMongoDb } from "./config/db.js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 // Access .env
 dotenv.config();
@@ -31,7 +26,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
 // routes
 app.use("/api/v1/auth", authRoutes);
@@ -39,8 +33,8 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/order", ordersRoutes);
 
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+app.get("/", (req, res) => {
+  res.send("Welcome to Timekart!");
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
